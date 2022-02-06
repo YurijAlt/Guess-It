682b9f9426ebcc128d54915eb80ade03adbefe78
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2022-02-06 19:00:24 +0300

    Initial Commit

---
+++ Geuess It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geuess It
+//
+//  Created by Юрий Альт on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
